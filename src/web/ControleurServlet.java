ff63d6b73399424f3e410d2cbe23aacace845c9a
Ismail BARKANI <ismail.barkani@um5s.net.ma>
2019-11-07 19:27:26 +0100

    creer le servlet et le deployer

--- src/web/ControleurServlet.java
+++ src/web/ControleurServlet.java
@@ -1,4 +1,4 @@
-package web.controleur;
+package web;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +14,7 @@ public class ControleurServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("test.jsp").forward(req,resp);
 
     }
 
