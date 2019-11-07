a110e1e8a10e990f099ff09fe145cb1bc3bc1233
Ismail BARKANI <ismail.barkani@um5s.net.ma>
2019-11-07 19:48:32 +0100

    Deployer le servlet par l'annotation @WebServlet

--- src/web/ControleurServlet.java
+++ src/web/ControleurServlet.java
@@ -1,11 +1,13 @@
 package web;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name="cs",urlPatterns={"*.ism","/ismail"})
 public class ControleurServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
